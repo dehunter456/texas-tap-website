cdf05368a900349c6fd0fb7aac85a158b38f2ef7
dehunter456 <dehunter456@gmail.com>
2019-05-16 18:17:34 -0500

    Updates

---
+++ precache-manifest.be91d561b270015c68746f1c6b143de2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/texas-tap-website/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "192616434dffa665e474",
+    "url": "/texas-tap-website/static/css/main.61afb786.chunk.css"
+  },
+  {
+    "revision": "f06a3227dbcff4d1f9ed",
+    "url": "/texas-tap-website/static/js/runtime~main.5c3e7480.js"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/texas-tap-website/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "c0f2d621d676eb4b6e9d",
+    "url": "/texas-tap-website/static/js/2.19d88e8d.chunk.js"
+  },
+  {
+    "revision": "b77098f737cf2ace9232e42ed7a60c33",
+    "url": "/texas-tap-website/static/media/about_placeholder-cropped.b77098f7.jpg"
+  },
+  {
+    "revision": "e078a20ed558cb796cef32f31a47e0a6",
+    "url": "/texas-tap-website/static/media/tt-logo.e078a20e.svg"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/texas-tap-website/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/texas-tap-website/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/texas-tap-website/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/texas-tap-website/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "192616434dffa665e474",
+    "url": "/texas-tap-website/static/js/main.38d14c64.chunk.js"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/texas-tap-website/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/texas-tap-website/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/texas-tap-website/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/texas-tap-website/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/texas-tap-website/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/texas-tap-website/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/texas-tap-website/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/texas-tap-website/static/media/fa-solid-900.f14c3b2f.ttf"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/texas-tap-website/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "c0f2d621d676eb4b6e9d",
+    "url": "/texas-tap-website/static/css/2.f0be969e.chunk.css"
+  },
+  {
+    "revision": "a91307293bcdb57894ea128f04888a79",
+    "url": "/texas-tap-website/index.html"
+  }
+];
